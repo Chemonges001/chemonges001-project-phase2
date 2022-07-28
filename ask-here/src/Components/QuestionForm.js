7c5086b279a9968e37aa045ca7532b4546074517
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-07-28 22:49:56 +0300

    update questionForm

--- ask-here/src/Components/QuestionForm.js
+++ ask-here/src/Components/QuestionForm.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react'
 function QuestionForm({onAdd}) {
   const [quiz, setQuiz] = useState('')
   const [category, setCategory] = useState('')
-  // const [answer, setAnswer] = useState('')
+
 
   const onSubmit = (e) => {
     e.preventDefault()
@@ -15,7 +15,7 @@ function QuestionForm({onAdd}) {
     onAdd( {quiz, category, } )
     setQuiz ('')
     setCategory('')
-    // setAnswer('')
+    
   }
   return (
     <form className='add-form' onSubmit={onSubmit}>
@@ -30,12 +30,7 @@ function QuestionForm({onAdd}) {
           <input type='text' placeholder='Add category' value ={category} onChange ={(e)=>
           setCategory(e.target.value)}/>
         </div>
-        {/* <div className ="form-control">
-          <label>Answers</label>
-          <input type='text' placeholder='Add an answer'value ={answer} onChange ={(e)=>
-          setAnswer(e.target.value)}
-          />
-        </div> */}
+      
         <input type= 'submit'  value = 'Save Question' className='btn btn-block'/>
     </form>
   )
